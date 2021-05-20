2d4a947c00363aeb13455eec117f754d4bb3b5ed
Yann Klein <yann.klein@me.com>
2021-05-20 12:57:12 +0900

    watch list done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :new, :show]
+  resources :lists do 
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
